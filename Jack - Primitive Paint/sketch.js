445333aa7d9c67c58806ff911fe710d9f3320476
JackAndrew2002 <jackandrew2002@gmail.com>
2019-02-15 11:10:07 -0600

    Jack - Primitive Paint

--- Jack - Primitive Paint/sketch.js
+++ Jack - Primitive Paint/sketch.js
@@ -5,25 +5,39 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x = mouseX;
-let y = mouseY;
+let x;
+let y;
 let name = 'Jack Andrew'
-let state = 0;
-const rectSize = 30
-let p = 0
-let q = 0
+let rectSize = 50;
+let rectSpeed = -1;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 0);
+  x = mouseX
+  y = mouseY
 }
+
+
 
 function draw() {
+//Creates a moving rectangle
+  rect(windowWidth/2, windowHeight/2, rectSize, rectSize);
+  if(rectSize > 100 || rectSize < 50){
+    rectSpeed *= -1;
+  }
+  rectSize += rectSpeed;
 
+//Creates Text with name on it
+  fill(50,205,50);
+  textSize(30);
+  textFont('Georgia');
+  text('Jack Andrew :)', windowWidth - 250, windowWidth - 10);
 }
 
-keyTyped();
 
+//Allows person to use keys a, s and d to make different shapes with random colours
 function keyTyped() {
   if (key === 'a') {
     fill(random(1, 255), random(1, 255), random(1, 255));
@@ -37,6 +51,11 @@ function keyTyped() {
     fill(random(1, 255), random(1, 255), random(1, 255));
     triangle(mouseX += 20, mouseY += 20, mouseX -= 20, mouseY -= 20, mouseX += 20, mouseY -= 20);
   }
+  else if (key === " "){
+    background(0)
+  }
+
 }
+
 
 
