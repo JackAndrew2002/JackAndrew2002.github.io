f5dfdbd77bd201233eb7c7d89c3e5a5bff811bdd
JackAndrew2002 <jackandrew2002@gmail.com>
2019-02-27 10:34:03 -0600

    Jack - Rollovers

--- Jack - Rollovers/sketch.js
+++ Jack - Rollovers/sketch.js
@@ -26,11 +26,22 @@ function draw() {
   rect(width/2, height/2, rectW, rectH);
 
 if (mouseX <= width/2 && mouseY <= height/2){
-  fill(255)
+  fill(0);
+  rect(0, 0, rectW, rectH);
 }
- 
-
-
+if (mouseX <= width/2 && mouseY >= height/2){
+  fill(0);
+  rect(0, height/2, rectW, rectH);
+}  
+if (mouseX >= width/2 && mouseY <= height/2){
+  fill(0);
+  rect(width/2, 0, rectW, rectH);
+  
+}
+if (mouseX >= width/2 && mouseY >= height/2){
+  fill(0);
+  rect(width/2, height/2, rectW, rectH);
+} 
 
   
 }
